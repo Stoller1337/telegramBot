9a53e4d6e3b9ea1b28a9aaddd5d85b3a6110c278
Stoller1337 <grisharul02@gmail.com>
2022-05-10 16:24:33 +0300

    update bot
    this work

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-from ast import keyword
-from turtle import delay
-from asyncio.windows_events import NULL
+import emoji
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 import telebot
@@ -17,7 +15,9 @@ date = 1
 
 @bot.message_handler(commands=['start'])
 def start(message: types.Message):
-     bot.send_message(message.chat.id, "Привет! Я бот Яндекс-Новости.\n\nНажмите на кнопку 'Новый запрос' для того что бы получить информацию.", reply_markup=bt.mainMenu)
+     bot.send_message(message.chat.id, "Привет! Я бот Яндекс-Новости.\n\n"
+                                       "Нажмите на кнопку 'Новый запрос' для того что бы получить информацию.",
+                      reply_markup=bt.mainMenu)
 
 
 print("key до функции request = " + key_word)
@@ -50,29 +50,28 @@ print("key до функции date = " + key_word)
 def choose_date(message: types.Message):
 
     global date
+    global date_num
     date = message.text
 
-    if date == 'За все время':
-        ans_date = 'За все время'
-    elif date == 'Сегодня':
-        ans_date = 'Сегодня'
-    elif date == '3 дня':
-        ans_date = '3 дня'
-    elif date == 'Неделя':
-        ans_date = 'Неделя'
+    match date:
+        case 'За все время':
+            ans_date = 'За все время'
+            date_num = 1
+        case 'Сегодня':
+            ans_date = 'Сегодня'
+            date_num = 2
+        case '3 дня':
+            ans_date = '3 дня'
+            date_num = 3
+        case 'Неделя':
+            date_num = 4
+            ans_date = 'Неделя'
 
     #string = "Новость: " + keyword + "\nПериод: " + date
     
     msg1 = bot.send_message(message.chat.id, "Новость: " + key + "\nПериод: " + ans_date)
-    bot.register_next_step_handler(msg1, parsingRequest)
-    array = ['[Google](http://www.google.com/)', '[VK](https://www.vk.com/)']
-    for i in range (len(array)):
-        bot.send_message(message.chat.id, array[i] , parse_mode='Markdown')
-
-print("key после функции date = " + key_word)
-
-def parsingRequest(message: types.Message):
-    
+    #bot.register_next_step_handler(msg1, parsingRequest)
+    bot.send_message(message.chat.id, 'Обрабатываю запрос...Пожалуйста подождите ' + '⏳')
     driver = webdriver.Firefox()
     driver.get("https://yandex.ru/news")
     if driver.title == 'Ой!':
@@ -81,32 +80,15 @@ def parsingRequest(message: types.Message):
         driver.implicitly_wait(1)
 
     request = key
-    #print("Колличество выводимых новостей: ", end="")
-    temp  = 5
-
-    # for i in range(len(temp)):
-    #     if temp[i] < '0' or temp[i] > '9':
-    #         print("Введено не число")
-    #         exit(0)
-
+    temp = 5
     count = int(temp)
-    #
-    # if count < 1:
-    #     print("Неверное колличество новостей")
-    #     exit(0)
+    choosePeriod = date_num
+    if choosePeriod < 1 or choosePeriod > 4:
+        bot.send_message(message.chat.id, "Неверный периода\nВведите запрос корректно",
+                                                            reply_markup=bt.mainMenu)
+        driver.quit()
 
-    print("Выберите перод:","1) За всё время","2) Сегодня", "3) 3 дня", "4) Неделя", sep="\n")
-    print("Введите номер (1-4): ", end="")
-    temp = date
-
-    for i in range(len(temp)):
-        if temp[i] < '1' or temp[i] > '4':
-            print("Неверный номер периода")
-            exit(0)
-
-    choosePeriod = int(temp)
-
-    text_box    = driver.find_element(By.CLASS_NAME, "input__control")
+    text_box = driver.find_element(By.CLASS_NAME, "input__control")
     find_button = driver.find_element(By.CLASS_NAME, "websearch-button")
 
     text_box.send_keys(request)
@@ -116,7 +98,7 @@ def parsingRequest(message: types.Message):
 
     if choosePeriod > 1:
         periods = driver.find_elements(By.CLASS_NAME, "Radiobox-Control")
-        period = periods[choosePeriod-1]
+        period = periods[choosePeriod - 1]
         period.click()
 
     news = driver.find_elements(By.CLASS_NAME, "news-search-story")
@@ -124,26 +106,113 @@ def parsingRequest(message: types.Message):
     if len(news) == 0:
         print("Новостей по такому запросу не найдено")
         driver.quit()
-        exit(0)
+        ##TODO
 
-    if len(news) < count:
-        count = len(news)
-
-    newsItem = news.pop(0)
-
-    print()
+    dictOfNews = {}
 
     for i in range(count):
-        bot.send_message(message.chat.id, "request is complet")
-        bot.send_message(message.chat.id, str(i+1, ') ', newsItem.find_element(By.CLASS_NAME, "mg-snippet__title").text))
-        print(i+1, ') ', newsItem.find_element(By.CLASS_NAME, "mg-snippet__title").text)
-        print(newsItem.find_element(By.CLASS_NAME, "mg-snippet__url").get_attribute("href"))
-        print()
-        newsItem = news.pop(0)
+        someNews = news.pop(0)
+        dictOfNews[someNews.find_element(By.CLASS_NAME, "mg-snippet__title").text] = someNews.find_element(
+            By.CLASS_NAME, "mg-snippet__url").get_attribute("href")
 
-    
 
-    driver.quit()
+    dictOfNews2 = []
+    for Name, Link in dictOfNews.items():
+        word = '[' + Name + ']' + '(' + Link + ')' + '\n'
+        dictOfNews2.append(word)
+        # dictOfNews2.append(Name)
+        # dictOfNews2.append(']')
+
+        # dictOfNews2.append(Link)
+
+    for i in range(len(dictOfNews2)):
+        print(dictOfNews2[i], '\n')
+    # print(dictOfNews.items())
+    # print(dictOfNews2)
+
+    for i in range(len(dictOfNews2)):
+        bot.send_message(message.chat.id, dictOfNews2[i], parse_mode='Markdown', reply_markup=bt.mainMenu)
+    # for i in range(len(dictOfNews2)):
+    #     # bot.send_message(message.chat.id, '\n'.join(Name, Link), reply_markup=bt.mainMenu)
+    #
+    #     bot.send_message(message.chat.id, dictOfNews2[i] + '\n', reply_markup=bt.mainMenu)
+
+
+print("key после функции date = " + key_word)
+
+# def parsingRequest(message: types.Message):
+#
+#     driver = webdriver.Firefox()
+#     driver.get("https://yandex.ru/news")
+#     if driver.title == 'Ой!':
+#         capcha = driver.find_element(By.CLASS_NAME, "CheckboxCaptcha")
+#         capcha.find_element(By.CLASS_NAME, "CheckboxCaptcha-Button").click()
+#         driver.implicitly_wait(1)
+#
+#     # request = key
+#     #print("Колличество выводимых новостей: ", end="")
+#     temp  = 5
+#
+#     # for i in range(len(temp)):
+#     #     if temp[i] < '0' or temp[i] > '9':
+#     #         print("Введено не число")
+#     #         exit(0)
+#
+#     count = int(temp)
+#     #
+#     # if count < 1:
+#     #     print("Неверное колличество новостей")
+#     #     exit(0)
+#
+#     print("Выберите перод:","1) За всё время","2) Сегодня", "3) 3 дня", "4) Неделя", sep="\n")
+#     print("Введите номер (1-4): ", end="")
+#     temp = date
+#
+#     for i in range(len(temp)):
+#         if temp[i] < '1' or temp[i] > '4':
+#             print("Неверный номер периода")
+#             exit(0)
+#
+#     choosePeriod = int(temp)
+#
+#     text_box    = driver.find_element(By.CLASS_NAME, "input__control")
+#     find_button = driver.find_element(By.CLASS_NAME, "websearch-button")
+#
+#     text_box.send_keys(request)
+#     find_button.click()
+#
+#     driver.implicitly_wait(3)
+#
+#     if choosePeriod > 1:
+#         periods = driver.find_elements(By.CLASS_NAME, "Radiobox-Control")
+#         period = periods[choosePeriod-1]
+#         period.click()
+#
+#     news = driver.find_elements(By.CLASS_NAME, "news-search-story")
+#
+#     if len(news) == 0:
+#         print("Новостей по такому запросу не найдено")
+#         driver.quit()
+#         exit(0)
+#
+#     if len(news) < count:
+#         count = len(news)
+#
+#     newsItem = news.pop(0)
+#
+#     print()
+#
+#     for i in range(count):
+#         bot.send_message(message.chat.id, "request is complet")
+#         bot.send_message(message.chat.id, str(i+1, ') ', newsItem.find_element(By.CLASS_NAME, "mg-snippet__title").text))
+#         print(i+1, ') ', newsItem.find_element(By.CLASS_NAME, "mg-snippet__title").text)
+#         print(newsItem.find_element(By.CLASS_NAME, "mg-snippet__url").get_attribute("href"))
+#         print()
+#         newsItem = news.pop(0)
+#
+#
+#
+#     driver.quit()
 
 bot.polling()
 
